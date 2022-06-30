3a57eb8140b2d5c84ab8bedf6b8ffb1071a136ff
kokinas <anton44192@gmail.com>
2022-06-30 17:48:34 +0500

    fix repeate unit, make reformat-code by Pycharm

--- Lesson_2/Task_2.4.py
+++ Lesson_2/Task_2.4.py
@@ -1,8 +1,8 @@
-#Home task Basics python
+# Home task Basics python
 ###Lesson #2
-#Task #2.4 split and handle message
+# Task #2.4 split and handle message
 
 in_str = input('type some message: ')
 str_handled = in_str.split()
 for i, word in enumerate(str_handled, 1):
-	print(i, word[:10])
+    print(i, word[:10])
